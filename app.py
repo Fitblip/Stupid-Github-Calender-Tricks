6f87be3be81081fcecc6681dd698053d4817084a
Fitblip <fitblip@gmail.com>
2014-05-06 23:32:37 -0700

    Add a new endpoint to dump the proper format data from github.
    Sometimes I feel like 80% of my development career is writing middleware.

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import json
+import time
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/data.json')
+def data():
+    with open('contributions_calendar_data.json') as f:
+        data = json.loads(f.read())
+    # Covert the list of lists github gives us to a dict with timestamps
+
+    timeobj = {}
+
+    for item in data:
+        timeStamp = time.strptime(item[0],'%Y/%m/%d')
+        timeStamp = time.mktime(timeStamp)
+        timeobj[timeStamp] = item[1]
+
+    return json.dumps(timeobj)
 
 if __name__ == '__main__':
     app.run(debug=True)
